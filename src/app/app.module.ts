acd09389d948de2bfc5946daa605c4e88f3e5cbc
sumaiyaasif <sumaiya.aa@gmail.com>
2021-03-28 22:06:24 -0500

    removed unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
-import { NavigationComponent } from './navigation/navigation.component';
 import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, AboutComponent],
+  declarations: [AppComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
